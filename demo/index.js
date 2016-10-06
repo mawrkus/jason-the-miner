ae198a8ff5c99f6a44e5e45fdcba650299b81fed
mawrkus <web@sparring-partner.be>
2016-10-07 00:59:15 +0200

    Updated demos

--- demo/index.js
+++ demo/index.js
@@ -23,20 +23,34 @@ const demoFiles = [
   // path.join(process.cwd(), 'demo/config/goodreads-http.json'),
   // path.join(process.cwd(), 'demo/config/imdb-file.json'),
   // path.join(process.cwd(), 'demo/config/imdb-http-paginate.json'),
-  // path.join(process.cwd(), 'demo/config/mixcloud-email.json'),
   // path.join(process.cwd(), 'demo/config/npm-tpl.json'),
+  // path.join(process.cwd(), 'demo/config/mixcloud-email.json'),
   // curl http://rickandmorty.wikia.com/wiki/Category:Characters | npm run demo:debug
   // OR cat demo/data/in/RickAndMorty@Wikia.html | npm run demo:debug
-  path.join(process.cwd(), 'demo/config/wikia-stdin.json')
+  // path.join(process.cwd(), 'demo/config/wikia-stdin.json')
 ];
 
 /* eslint-disable */
 
 const demoSequenceP = demoFiles.reduce((previousP, file) => {
-  return previousP.then(() => jason.loadConfig(file).then(() => jason.harvest()));
+  return previousP
+          .then(() => jason.loadConfig(file))
+          .then(() => jason.harvest());
 }, Promise.resolve());
 
 demoSequenceP
+  .then(() => {
+    const npmConfigFile = path.join(process.cwd(), 'demo/config/npm-tpl.json');
+    const outputConfig = {
+      "tpl": {
+        "templatePath": "demo/data/in/npm-starred.md.tpl",
+        "outputPath": "demo/data/out/npm-starred.md"
+      }
+    };
+
+    jason.loadConfig(npmConfigFile)
+      .then(() => jason.harvest({ outputConfig }));
+  })
   .then(() => console.log('\nAll done! :D'))
   .catch(error => {
     console.error('Ooops! Something went wrong. :(');
